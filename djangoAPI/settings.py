aad2a6ee3dc1f64a6cb3ca685affabf4d6592067
Anonymous One <anonymousone@Anonymous-MB-Pro.local>
2019-02-06 03:08:56 -0500

    Testing static files

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -134,13 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_URL = '/static/'
 
 # FOR DJANGO REST FRAMEWORK PAGINATION
 REST_FRAMEWORK = {
