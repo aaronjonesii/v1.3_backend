3d1dbe3bfcb2bb62f3dd8002b47b59f331461df2
Anonymous One <anonymousone@Anonymous-MB-Pro.local>
2019-02-06 03:23:08 -0500

    Testing static files with whitenoise

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'djangoAPI.urls'
