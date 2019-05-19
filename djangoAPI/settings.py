8122166636cc0aa0df418f2c3aef90200fce5ad1
Aaron Jones <ajones@ajones-macpro.local>
2019-05-19 19:57:00 -0400

    Log to stream

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -223,3 +223,19 @@ CHANNEL_LAYERS = {
 #        },
 #    },
 # }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
